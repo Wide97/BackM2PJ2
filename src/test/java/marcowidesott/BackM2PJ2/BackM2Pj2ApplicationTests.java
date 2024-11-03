0e0290b3404bba8421d024465c5610887ba9c602
Wide97 <mwidesott@gmail.com>
2024-11-03 11:53:49 +0100

    init

---
+++ src/test/java/marcowidesott/BackM2PJ2/BackM2Pj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package marcowidesott.BackM2PJ2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackM2Pj2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
