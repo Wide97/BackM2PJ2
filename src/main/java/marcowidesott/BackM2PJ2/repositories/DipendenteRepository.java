70b2d7940ea5baee84b65bbdc5adbc6e7bc0b362
Wide97 <mwidesott@gmail.com>
2024-11-03 16:03:44 +0100

    init

---
+++ src/main/java/marcowidesott/BackM2PJ2/repositories/DipendenteRepository.java
@@ -0,0 +1,7 @@
+package marcowidesott.BackM2PJ2.repositories;
+
+import marcowidesott.BackM2PJ2.entities.Dipendente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DipendenteRepository extends JpaRepository<Dipendente, Long> {
+}
---
+++ src/main/java/marcowidesott/BackM2PJ2/repositories/ViaggioRepository.java
@@ -0,0 +1,7 @@
+package marcowidesott.BackM2PJ2.repositories;
+
+import marcowidesott.BackM2PJ2.entities.Viaggio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ViaggioRepository extends JpaRepository<Viaggio, Long> {
+}
