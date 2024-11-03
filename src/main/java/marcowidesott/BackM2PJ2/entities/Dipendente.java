749bc54e36108f1f23a45ce252fdf62bcda1b0d4
Wide97 <mwidesott@gmail.com>
2024-11-03 12:42:18 +0100

    init

---
+++ src/main/java/marcowidesott/BackM2PJ2/entities/Dipendente.java
@@ -0,0 +1,30 @@
+package marcowidesott.BackM2PJ2.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class Dipendente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String nome;
+    private String cognome;
+    private String email;
+
+    public Dipendente(String email, String cognome, String nome, String username) {
+        this.email = email;
+        this.cognome = cognome;
+        this.nome = nome;
+        this.username = username;
+    }
+}
